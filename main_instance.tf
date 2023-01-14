a27c1aa0070d5bb51f80d35519850b024a47d870
tribuivan198 <122589391+tribuivan198@users.noreply.github.com>
2023-01-14 19:52:42 +0000

    update

--- main_instance.tf
+++ main_instance.tf
@@ -1,5 +1,16 @@
-resource "vultr_instance" "my_instance" {
-    plan = "vc2-1c-1gb"
-    region = "sea"
-    os_id = 167
+resource "vultr_instance" "ethan_instance" {
+  plan   = "vc2-1c-1gb"
+  region = "sgp"
+  #os_id       = 167
+  image_id = 5
+  label    = "my-instance-label"
+  tags     = ["my-instance-tag"]
+  hostname = "my-instance-hostname"
+  #enable_ipv6 = true
+  backups = "enabled"
+  backups_schedule {
+    type = "daily"
+  }
+  #ddos_protection  = true
+  activation_email = false
 }
--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 terraform {
-  
+
 }
 
 terraform {
   required_providers {
     vultr = {
-      source = "vultr/vultr"
+      source  = "vultr/vultr"
       version = "2.12.0"
     }
   }
@@ -20,8 +20,8 @@ terraform {
 
 # Configure the Vultr Provider
 provider "vultr" {
-  api_key = var.VULTR_API_KEY
-  rate_limit = 100
+  api_key     = var.VULTR_API_KEY
+  rate_limit  = 100
   retry_limit = 3
 }
 
